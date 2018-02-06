3fcf3e6915f136e2c5228277010ec15a36901b24
github username <github email address>
2018-02-05 22:03:42 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,72 @@
+INSERT INTO users (name, age) VALUES ("John Doe", 55);
+INSERT INTO users (name, age) VALUES ("John Doe", 55);
+INSERT INTO users (name, age) VALUES ("John Doe", 55);
+INSERT INTO users (name, age) VALUES ("John Doe", 55);
+INSERT INTO users (name, age) VALUES ("John Doe", 55);
+INSERT INTO users (name, age) VALUES ("John Doe", 55);
+INSERT INTO users (name, age) VALUES ("John Doe", 55);
+INSERT INTO users (name, age) VALUES ("John Doe", 55);
+INSERT INTO users (name, age) VALUES ("John Doe", 55);
+INSERT INTO users (name, age) VALUES ("John Doe", 55);
+INSERT INTO users (name, age) VALUES ("John Doe", 55);
+INSERT INTO users (name, age) VALUES ("John Doe", 55);
+INSERT INTO users (name, age) VALUES ("John Doe", 55);
+INSERT INTO users (name, age) VALUES ("John Doe", 55);
+INSERT INTO users (name, age) VALUES ("John Doe", 55);
+INSERT INTO users (name, age) VALUES ("John Doe", 55);
+INSERT INTO users (name, age) VALUES ("John Doe", 55);
+INSERT INTO users (name, age) VALUES ("John Doe", 55);
+INSERT INTO users (name, age) VALUES ("John Doe", 55);
+INSERT INTO users (name, age) VALUES ("John Doe", 55);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("card game", "Gaming", 15000, "Jan 9", "Feb 9");
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("honeymoon", "Trip", 15000, "Jan 1", "Feb 1");
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("board game", "Gaming", 1000, "Jan 9", "Feb 9");
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("camera", "Product", 2000, "Jan 9", "Feb 9");
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("whatever", "Gaming", 1000, "Jan 9", "Feb 9");
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("rock climbing feats", "Sports", 19000, "Jan 9", "Feb 9");
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("eating contest", "Sponsorship", 15000, "Jan 9", "Feb 9");
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("comic book", "Gaming", 15000, "Jan 9", "Feb 9");
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("card game", "Gaming", 15000, "Jan 9", "Feb 9");
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("card game", "Gaming", 15000, "Jan 9", "Feb 9");
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES
+(1, 3, 4),
+(1, 3, 5),
+(1, 3, 4),
+(1, 3, 5),
+(1, 3, 4),
+(1, 3, 5),
+(1, 3, 4),
+(1, 3, 5),
+(1, 3, 4),
+(1, 3, 5),
+(1, 3, 4),
+(1, 3, 5),
+(1, 3, 4),
+(1, 3, 5),
+(1, 3, 4),
+(1, 3, 5),
+(1, 3, 4),
+(1, 3, 5),
+(1, 3, 4),
+(1, 3, 5),
+(1, 3, 4),
+(1, 3, 5),
+(1, 3, 4),
+(1, 3, 5),
+(2, 4, 8),
+(1, 3, 5),
+(1, 3, 4),
+(1, 3, 5),
+(2, 4, 8),
+(9, 9, 7);
